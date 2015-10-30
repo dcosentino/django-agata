c648ae510765d06578b493ca6a7e1acc72dacb67
Domenico Cosentino <d.cosentino@4sigma.it>
2015-10-30 18:22:23 +0000

    Initial commit

---
+++ src/agata/urls.py
@@ -0,0 +1,9 @@
+#-*- coding: utf-8 -*-
+
+from django.conf.urls import patterns, include, url
+
+from django.conf import settings
+
+# Include here all the urls.py files from the various apps
+urlpatterns = patterns('',
+)
